6e006241c28c1dad4f16f1a417d4492eadf8b60f
Anthony Guérot <anthony@digikare.com>
2022-04-22 19:00:23 +0200

    feat(user) controller cqrs + tests

--- apps/api/src/app/user.controller.spec.ts
+++ apps/api/src/app/user.controller.spec.ts
@@ -1,13 +1,29 @@
+import Substitute, { SubstituteOf } from '@fluffy-spoon/substitute';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { Test, TestingModule } from '@nestjs/testing';
+import { UserBuilder } from './test/user.builder';
+import { CreateUser } from './usecase/create-user/create-user.command';
+import { DeleteUser } from './usecase/delete-user/delete-user.command';
+import { GetUserById } from './usecase/get-user-by-id/get-user-by-id.command';
+import { GetUsers } from './usecase/get-users/get-users.query';
 import { UserController } from './user.controller';
 
 describe('user controller', () => {
   let app: TestingModule;
   let controller: UserController;
+  let queryBus: SubstituteOf<QueryBus>;
+  let commandBus: SubstituteOf<CommandBus>;
 
   beforeAll(async () => {
+    queryBus = Substitute.for<QueryBus>();
+    commandBus = Substitute.for<CommandBus>();
+
     app = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [
+        { provide: QueryBus, useValue: queryBus },
+        { provide: CommandBus, useValue: commandBus },
+      ]
     }).compile();
 
     controller = app.get<UserController>(UserController);
@@ -15,6 +31,11 @@ describe('user controller', () => {
 
 
   it('should get all users', () => {
+    // Arrange
+    queryBus.execute(new GetUsers()).resolves([
+      UserBuilder.create('id1'),
+      UserBuilder.create('id2')
+    ]);
 
     // Act
     const result = controller.getAll();
@@ -24,37 +45,93 @@ describe('user controller', () => {
   });
 
   describe('get by id', () => {
-    it('should get user by id', () => {
+    it('should get user by id', async () => {
+      // Arrange
+      queryBus.execute(new GetUserById('user1')).resolves(UserBuilder.create('user1'));
+
       // Act
-      const result = controller.getById('user1');
+      const result = await controller.getById('user1');
 
       // Assert
-      expect(result).toStrictEqual({ id: 'user1', email: 'user1@inter.net' });
+      expect(result).toMatchObject({
+        id: 'user1',
+        email: 'user1@inter.net',
+        consents: []
+      });
+    });
+
+    it('should throw 422 on unknown user', async () => {
+      // Arrange
+      queryBus.execute(new GetUserById('user1')).resolves(undefined);
+
+      // Act
+      const getById = () => controller.getById('user1');
+
+      // Assert
+      expect(getById())
+        .rejects
+        .toThrow('Unprocessable Entity');
     });
 
   });
 
   describe('create new user', () => {
-    it('should create new user', () => {
+    it('should create new user', async () => {
+      // Arrange
+      commandBus.execute(new CreateUser('user1@inter.net'))
+        .resolves(UserBuilder.create('user1'));
+
       // Act
-      const result = controller.create({ email: 'user1@inter.net' });
+      const result = await controller.create({ email: 'user1@inter.net' });
+
+      console.log(JSON.stringify(result));
 
       // Assert
-      expect(result).toStrictEqual({
-        id: '000000',
+      expect(result).toMatchObject({
+        id: 'user1',
         email: 'user1@inter.net',
         consents: []
       });
     });
+
+    it('should throw 422 on duplicate email', async () => {
+      // Arrange
+      commandBus.execute(new CreateUser('user1@inter.net'))
+        .resolves(undefined);
+
+      // Act
+      const result = () => controller.create({ email: 'user1@inter.net' });
+
+      // Assert
+      expect(result())
+      .rejects
+      .toThrow('Unprocessable Entity');
+    });
   });
 
   describe('delete user', () => {
-    it('should delete existing user', () => {
+    it('should delete existing user', async () => {
+      // Arrange
+      commandBus.execute(new DeleteUser('user1')).resolves(true);
+
       // Act
-      const result = controller.deleteByid('user1');
+      const result = await controller.deleteByid('user1');
 
       // Assert
-      expect(result).toBeTruthy();
+      expect(result).toBe(true);
+    });
+
+    it('should throw 422 on unknown userId', async () => {
+      // Arrange
+      commandBus.execute(new DeleteUser('user1')).resolves(false);
+
+      // Act
+      const result = () => controller.deleteByid('user1');
+
+      // Assert
+      expect(result())
+        .rejects
+        .toThrow('Unprocessable Entity');
     });
   });
 });
--- apps/api/src/app/user.controller.ts
+++ apps/api/src/app/user.controller.ts
@@ -1,31 +1,52 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, UnprocessableEntityException } from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { User } from './domain/user';
+import { CreateUser } from './usecase/create-user/create-user.command';
+import { DeleteUser } from './usecase/delete-user/delete-user.command';
+import { GetUserById } from './usecase/get-user-by-id/get-user-by-id.command';
+import { GetUsers } from './usecase/get-users/get-users.query';
 
 @Controller('users')
 export class UserController {
+
+  constructor(
+    private readonly _commandBus: CommandBus,
+    private readonly _queryBus: QueryBus) {
+  }
+
   @Get()
-  getAll(): any[] {
-    return [
-      { id: 'user1', email: 'user1@inter.net' },
-      { id: 'user2', email: 'user2@inter.net' }
-    ];
+  getAll(): Promise<User[]> {
+    return this._queryBus.execute<GetUsers, User[]>(new GetUsers());
   }
 
   @Get(':id')
-  getById(@Param('id') id: string): any {
-    return { id, email: `${id}@inter.net` };
+  async getById(@Param('id') id: string): Promise<User> {
+    const user = await this._queryBus.execute<GetUserById, User>(new GetUserById(id));
+    if (!user) {
+      throw new UnprocessableEntityException();
+    }
+
+    return user;
   }
 
   @Post()
-  create(@Body() { email }: { email: string }) {
-    return {
-      id: '000000',
-      email: email,
-      consents: []
-    };
+  async create(@Body() { email }: { email: string }) {
+    const user = await this._commandBus.execute<CreateUser, User>(new CreateUser(email));
+
+    if (!user) {
+      throw new UnprocessableEntityException();
+    }
+
+    return user;
   }
 
   @Delete(':id')
-  deleteByid(@Param('id') id: string) {
-    return true;
+  async deleteByid(@Param('id') id: string) {
+    const success = await this._commandBus.execute<DeleteUser, boolean>(new DeleteUser(id));
+    if (!success) {
+      throw new UnprocessableEntityException();
+    }
+
+    return success;
   }
 }
