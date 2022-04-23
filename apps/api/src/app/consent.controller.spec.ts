51f9bb9a2c2322398b4e7ea84f63ef1754e1d9e6
Anthony Guérot <anthony@digikare.com>
2022-04-23 08:08:26 +0200

    feat(consent) crqs controller + tests

--- apps/api/src/app/consent.controller.spec.ts
+++ apps/api/src/app/consent.controller.spec.ts
@@ -1,30 +1,78 @@
+import Substitute, { Arg, SubstituteOf } from '@fluffy-spoon/substitute';
+import { CommandBus } from '@nestjs/cqrs';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ConsentController } from './consent.controller';
+import { Consent } from './domain/consent';
+import { AppendConsents } from './usecase/append-consent/append-consent.command';
 
 describe('consent controller', () => {
   let app: TestingModule;
   let controller: ConsentController;
+  let commandBus: SubstituteOf<CommandBus>;
+  let consents: Consent[];
 
-  beforeAll(async () => {
+  beforeEach(async () => {
+    commandBus = Substitute.for<CommandBus>();
+
     app = await Test.createTestingModule({
       controllers: [ConsentController],
+      providers: [
+        { provide: CommandBus, useValue: commandBus }
+      ]
     }).compile();
 
     controller = app.get<ConsentController>(ConsentController);
+
+    consents = [
+      { id: 'sms_notifications', enabled: true }
+    ];
   });
 
 
-  it('should get all users', () => {
+  it('should append consents to existing user', async () => {
+    // Arrange
+    commandBus.execute(new AppendConsents('user1', consents))
+      .resolves(true);
 
     // Act
-    const result = controller.appendConsents({
+    const result = await controller.appendConsents({
+      user: { id: 'user1' },
+      consents
+    });
+
+    // Assert
+    expect(result).toBe(true);
+  });
+
+  it('should throw 422 on unknown userId', async () => {
+    // Arrange
+    commandBus.execute(new AppendConsents('user1', consents))
+      .resolves(false);
+
+    // Act
+    const result = () => controller.appendConsents({
+      user: { id: 'user1' },
+      consents
+    });
+
+    // Assert
+    expect(result()).rejects.toThrow('Unprocessable Entity');
+  });
+
+  it('should throw 422 on empty consents list', async () => {
+    // Arrange
+    commandBus.execute(new AppendConsents('user1', []))
+      .resolves(true);
+
+    // Act
+    const result = () => controller.appendConsents({
       user: { id: 'user1' },
       consents: [
       ]
     });
 
     // Assert
-    expect(result).toBeTruthy();
+    expect(result()).rejects.toThrow('Unprocessable Entity');
   });
 
 });
--- apps/api/src/app/consent.controller.ts
+++ apps/api/src/app/consent.controller.ts
@@ -1,9 +1,25 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UnprocessableEntityException } from '@nestjs/common';
+import { CommandBus } from '@nestjs/cqrs';
+import { Consent } from './domain/consent';
+import { AppendConsents } from './usecase/append-consent/append-consent.command';
 
 @Controller('consents')
 export class ConsentController {
+  constructor(private readonly _commandBus: CommandBus) {
+  }
+
   @Post()
-  appendConsents(@Body() dto: { user: { id: string }, consents: [] }) {
-    return true;
+  async appendConsents(@Body() dto: { user: { id: string }, consents: Consent[] }) {
+    if (!dto.consents || dto.consents.length === 0) {
+      throw new UnprocessableEntityException();
+    }
+
+    const success = await this._commandBus.execute<AppendConsents, boolean>(new AppendConsents(dto.user.id, dto.consents));
+
+    if(!success) {
+      throw new UnprocessableEntityException();
+    }
+
+    return success;
   }
 }
